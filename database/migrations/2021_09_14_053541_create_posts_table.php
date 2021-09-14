a4de9837c3328f965fa6f719ba07eb19b0a6b77b
Edd-war <digiespirit.1807@gmail.com>
2021-09-14 00:44:46 -0500

    Actualizado a clase del lunes 13 septiembre 2021

---
+++ database/migrations/2021_09_14_053541_create_posts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string("title", 50);
+            $table->string("author", 50);
+            $table->text("content");
+            $table->string("image", 100);
+            $table->unsignedBigInteger("category_id");
+            $table->foreign('category_id')
+                    ->references('id')
+                    ->on('categories');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
